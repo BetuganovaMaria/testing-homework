b0e3fa3b9f98a3936cadb56373525f8f7d718637
panzerpanzer28@gmail.com <panzerpanzer28@gmail.com>
2022-11-20 23:52:20 +0300

    first commit

---
+++ babel.config.js
@@ -0,0 +1,8 @@
+const presets = [
+    ["@babel/preset-env"],
+    ["@babel/preset-react", {runtime: "automatic"}],
+    "@babel/preset-flow",
+    "@babel/preset-typescript",
+];
+
+module.exports = {presets};
